902e9dc2174ffd326d448479d3229bf7972fbf1f
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-11-27 22:43:57 +0200

    Adding GET api

--- server-side/index.js
+++ server-side/index.js
@@ -30,6 +30,11 @@ async function run() {
     const database = client.db("userDB");
     const userCollection = database.collection("users");
 
+    app.get('/users',async(req,res)=>{
+      const cursor=userCollection.find()
+      const result=await cursor.toArray()
+      res.send(result)
+    })
     app.post('/users',async(req,res)=>{
         const userData=req.body
         console.log(userData)
